40f8fd8eb466e2a9113d2ba002026a89f863b7fe
Subham Mani <manisubham09@gmail.com>
2025-04-10 17:01:56 +0530

    refactor

--- app/src/actions/signup.ts
+++ app/src/actions/signup.ts
@@ -59,7 +59,7 @@ export async function signup(formData: FormData) {
     const { error } = await resend.emails.send({
       from: "Subham from Prompt Brawl <no-reply@updates.subhammani.xyz>",
       to: email,
-      subject: "Verify Your Email - Prompt Brawl",
+      subject: "Verify Your Email",
       react: await EmailTemplate({ otp: otp.toString() }),
       text: `Welcome Soldier!\nVerify your email to claim your username.\nYour OTP is: ${otp}\nThis OTP is valid for 10 minutes. Please do not share it with anyone.`,
     });
