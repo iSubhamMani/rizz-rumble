82e79e6971f37f69d8cca7c3895549bc6e8251c3
Subham Mani <manisubham09@gmail.com>
2025-05-03 20:26:58 +0530

    enhance RedisService: add subscriber for game:initialChallenge and expose Subscriber getter

--- worker/src/services/Redis.ts
+++ worker/src/services/Redis.ts
@@ -2,17 +2,25 @@ import { Redis } from "ioredis";
 
 class RedisService {
   private pub: Redis;
+  private sub: Redis;
   private queue: Redis;
   private store: Redis;
 
   constructor() {
     this.pub = new Redis(process.env.REDIS_URL!);
+    this.sub = new Redis(process.env.REDIS_URL!);
     this.queue = new Redis(process.env.REDIS_URL!);
     this.store = new Redis(process.env.REDIS_URL!);
+
+    this.sub.subscribe("game:initialChallenge");
   }
 
   public get Publisher() {
     return this.pub;
+  }
+
+  public get Subscriber() {
+    return this.sub;
   }
 
   public get Queue() {
